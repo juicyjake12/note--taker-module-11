65c929a58e878f1baf2fc25f2a543302c78cb92f
juicyjake12 <108505695+juicyjake12@users.noreply.github.com>
2022-10-10 21:12:40 -0400

    hopefully this fixes it

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-
-const express = require("express");
-const path = require("path");
-
-
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(express.static('public'));
 
-
-app.use(express.static("public"));
-
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
